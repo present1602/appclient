91f6a1701d35753195119fc7edd46c6a6edea0bc
kimcy <01191318447@naver.com>
2023-04-13 18:28:31 +0900

    img edit ui

--- src/views/biz/components/PortalImagesEdit.js
+++ src/views/biz/components/PortalImagesEdit.js
@@ -1,4 +1,4 @@
-import { Button } from 'components/ui';
+import { Button, CloseButton } from 'components/ui';
 import React from 'react';
 import { HiEye } from 'react-icons/hi';
 import { useSelector } from 'react-redux';
@@ -11,12 +11,37 @@ const PortlImagesEdit = () => {
   const navigate = useNavigate()
   const persistImages = useSelector((state) => state.bizPersistData.data.portal_images)
 
+  const removeFile = (index) => {
+
+  }
 
   return (
     <div className='h-full w-full'>
       <h5 className='my-6'>포털 이미지 수정</h5>
+      <div className='flex justify-end my-1'>
+        <Button onClick={() => { }} className='mx-1'>취소</Button>
+        <Button onClick={() => { }} className='mx-1'>저장</Button>
+      </div>
+      {persistImages.map((img, index) => (
+        <div className='upload-file w-full h-[120px] py-1 px-1'>
+          <div className='flex w-full h-full'>
+            <div className='h-full flex justify-center w-[180px]'>
+              <img src={img.full_path} className='h-full' />
+            </div>
+            <div className="upload-file-info">
+              {/* <h6 className="upload-file-name">{name}</h6> */}
+              <h6 className="upload-file-name">{img.filename}</h6>
+            </div>
+          </div>
+          <CloseButton
+            onClick={() => removeFile(index)}
+            className="upload-file-remove"
+          />
+        </div>
 
-      {persistImages.map((img) => (
+      ))}
+
+      {/* {persistImages.map((img) => (
         <div
           className="group relative rounded border p-2 flex"
           key={img.full_path}
@@ -31,19 +56,16 @@ const PortlImagesEdit = () => {
               onClick={() => { }}
               className="text-gray-100 hover:text-gray-300 cursor-pointer p-1.5"
             >
-              {/* <HiEye /> */}
               {img.filename}
             </span>
             <span
               onClick={() => { }}
               className="text-gray-100 hover:text-gray-300 cursor-pointer p-1.5"
             >
-              {/* <HiTrash /> */}
             </span>
           </div>
         </div>
-      ))}
-
+      ))} */}
     </div>
   );
 }
