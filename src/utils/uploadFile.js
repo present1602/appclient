a8a7b58e65774be680251b120ce57d8b6ce79845
kimcy <01191318447@naver.com>
2023-04-13 21:14:10 +0900

    shop file upload direct

---
+++ src/utils/uploadFile.js
@@ -0,0 +1,27 @@
+// import AWS from 'aws-sdk'
+import S3 from 'react-aws-s3'
+
+
+export function directUploadFile(file, dirName) {
+  // const s3 = new AWS.S3()
+
+  const s3_config = {
+    bucketName: process.env.REACT_APP_AWS_S3_BUCKET,
+    dirName: dirName,
+    region: process.env.REACT_APP_AWS_S3_REGION,
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  }
+
+  debugger;
+  const ReactS3Client = new S3(s3_config);
+  return new Promise(resolve => {
+    ReactS3Client
+      .uploadFile(file, Math.trunc(Math.random() * 100000) + file.name)
+      .then((data) => {
+        resolve(data)
+
+      })
+      .catch(err => console.error(err))
+  })
+}
--- src/views/biz/components/PortalImagesEdit.js
+++ src/views/biz/components/PortalImagesEdit.js
@@ -1,9 +1,10 @@
 import { Button, CloseButton } from 'components/ui';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { HiEye } from 'react-icons/hi';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { injectReducer } from 'store';
+import { directUploadFile } from 'utils/uploadFile';
 import reducer from '../store';
 
 injectReducer('bizPersistData', reducer)
@@ -11,8 +12,34 @@ const PortlImagesEdit = () => {
   const navigate = useNavigate()
   const persistImages = useSelector((state) => state.bizPersistData.data.portal_images)
 
+  const [fileListData, setFileListData] = useState(persistImages)
+
+  const bizKeyInfo = useSelector((state) => state.auth.session.bizKeyInfo)
+
+  const fileRef = useRef()
+
   const removeFile = (index) => {
 
+  }
+
+  const handleFileUpload = async (e) => {
+    const bizId = bizKeyInfo.bizId
+    const file = e.target.files[0]
+
+    const response = await directUploadFile(file, 'biz/portal')
+    debugger;
+    const newFileElement = {
+      'full_path': response.location,
+      'filename': `${bizId}_${Math.random()}_${file.name}`,
+      'original_filename': file.name,
+    }
+    debugger;
+    setFileListData([...fileListData, newFileElement])
+
+  }
+
+  function handleFileButtonClick() {
+    fileRef.current?.click()
   }
 
   return (
@@ -22,7 +49,8 @@ const PortlImagesEdit = () => {
         <Button onClick={() => { }} className='mx-1'>취소</Button>
         <Button onClick={() => { }} className='mx-1'>저장</Button>
       </div>
-      {persistImages.map((img, index) => (
+      {/* {persistImages.map((img, index) => ( */}
+      {fileListData.map((img, index) => (
         <div className='upload-file w-full h-[120px] py-1 px-1'>
           <div className='flex w-full h-full'>
             <div className='h-full flex justify-center w-[180px]'>
@@ -41,8 +69,9 @@ const PortlImagesEdit = () => {
 
       ))}
 
+      <input type="file" ref={fileRef} onChange={handleFileUpload} className='upload-input' />
       <div className='upload-file w-full h-[120px] py-1 px-1'>
-        <div className='flex w-full h-full' onClick={() => { }}>
+        <div className='flex w-full h-full' onClick={handleFileButtonClick}>
           <div className='h-full flex justify-center w-[180px] p-5'>
             <img src='/img/others/upload.png' className='h-full' />
           </div>
